85d6792aae3c1975ee554781a58aa6da699a7a59
Nathan Ortbals <nathan.ortbals@gmail.coma>
2019-03-12 12:28:11 -0500

    added files

--- ortbalsn-challenge-images/ViewController.swift
+++ ortbalsn-challenge-images/ViewController.swift
@@ -10,11 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func openCamera(_ sender: Any) {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            print("Camera not supported by this device")
+            return
+        }
+        
+        imagePicker.sourceType = .camera
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true)
+    }
+    
+    @IBAction func openPhotoLibrary(_ sender: Any) {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+            print("can't open photo library")
+            return
+        }
+        
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true)
+    }
+}
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        defer {
+            picker.dismiss(animated: true)
+        }
+        
+        guard let image = info[.originalImage] as? UIImage else {
+            return
+        }
+        
+        imageView.image = image
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        defer {
+            picker.dismiss(animated: true)
+        }
+        
+        print("did cancel")
+    }
 }
 
