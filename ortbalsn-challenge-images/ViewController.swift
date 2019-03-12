f0b010b50bc5447399eca1d0bf263b13f91b79e6
Nathan Ortbals <nathan.ortbals@gmail.coma>
2019-03-12 11:41:47 -0500

    Initial Commit

---
+++ ortbalsn-challenge-images/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ortbalsn-challenge-images
+//
+//  Created by Nathan Ortbals on 3/12/19.
+//  Copyright © 2019 Nathan Ortbals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ortbalsn-challenge-imagesTests/ortbalsn_challenge_imagesTests.swift
@@ -0,0 +1,34 @@
+//
+//  ortbalsn_challenge_imagesTests.swift
+//  ortbalsn-challenge-imagesTests
+//
+//  Created by Nathan Ortbals on 3/12/19.
+//  Copyright © 2019 Nathan Ortbals. All rights reserved.
+//
+
+import XCTest
+@testable import ortbalsn_challenge_images
+
+class ortbalsn_challenge_imagesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
